d9919bd27774cfee6b644f0e9b19e996275f5394
durukanoktay <durukanoktay81@gmail.com>
2024-06-24 08:14:15 +0300

    init

---
+++ QCustomPlot_intro/src/networkplot.cpp
@@ -0,0 +1,14 @@
+#include "networkplot.h"
+#include "ui_networkplot.h"
+
+NetworkPlot::NetworkPlot(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::NetworkPlot)
+{
+    ui->setupUi(this);
+}
+
+NetworkPlot::~NetworkPlot()
+{
+    delete ui;
+}
